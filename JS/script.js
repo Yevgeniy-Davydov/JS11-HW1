93a0dd255d9ec38c428a80ccab460ebb6eda9071
Yevgeniy Davydov <yevgeniydavydov@Yevgeniys-MBP.fritz.box>
2024-01-15 10:39:45 +0100

    new files added

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+let x = 2;
+let y = 4;
+
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+let power = x ** 2;
+
+console.log("додавання: ", sum);
+console.log("віднімання: ", difference);
+console.log("множення: ", product);
+console.log("ділення: ", quotient);
+console.log("зведення в ступінь: ", power);
